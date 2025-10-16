212ae7cbc2744a7e2009e0b0ffe9991b49d5a29b
namit-sentry-skin <namit@sentryskin.ai>
2025-10-16 13:28:26 -0400

    Configure Vite publicDir explicitly to fix font loading on Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
   server: {
     port: 3000,
+  },
+  build: {
+    assetsInlineLimit: 0,
   },
 })
 
